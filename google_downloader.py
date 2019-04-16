f1e957320c16f58efa0f75d9d88c7b5cea0ce8b3
SungHoon Im <shi510.kor@gmail.com>
2019-04-16 15:12:05 +0900

    rename variables.

--- google_downloader.py
+++ google_downloader.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--search", required=True)
     args = parser.parse_args()
-    people = args.search
+    targets = args.search
 
     headers = {'User-Agent': 
                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36'
@@ -16,20 +16,20 @@ def main():
                }
     search_url = "https://www.google.com/search?"
     re_expr = re.compile('<img id.+?<div class.+?"ou":"(.+?)".+?:.+?</div>')
-    for p in people.split(','):
-        pq = quote(p)
-        img_url =  search_url + 'q={}&tbm=isch'.format(pq)
+    for t in targets.split(','):
+        quoted = quote(t)
+        img_url =  search_url + 'q={}&tbm=isch'.format(quoted)
         img_req = Request(url=img_url, headers=headers)
         img_html = urlopen(img_req).read().decode('utf-8')
         img_srcs = re_expr.findall(img_html)
-        os.mkdir(p)
+        os.mkdir(t)
         for i in enumerate(img_srcs):
             try:
-                t = urlopen(Request(url=i[1], headers=headers))
+                img = urlopen(Request(url=i[1], headers=headers))
                 print(i[1])
-                filename = p + '/{}_{}.jpg'.format(p,str(i[0]+1))
+                filename = t + '/{}_{}.jpg'.format(t,str(i[0]+1))
                 with open(filename,"wb") as f:
-                    f.write(t.read())
+                    f.write(img.read())
             except:
                 pass
 if __name__=="__main__":
