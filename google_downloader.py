01c14dbeb01bcf2e8e43236046f71857acaf7f95
Im Sunghoon <shi510.kor@gmail.com>
2021-05-31 15:36:38 +0900

    Update

--- google_downloader.py
+++ google_downloader.py
@@ -36,6 +36,8 @@ def download_chromedriver(version):
         root += '89.0.4389.23/'
     elif version == 90:
         root += '90.0.4430.24/'
+    elif version == 91:
+        root += '91.0.4472.19/'
     else:
         raise 'Wrong chrome version : ' + version
 
@@ -69,20 +71,21 @@ def get_browser(args):
     browser = webdriver.Chrome('./chromedriver', chrome_options=options)
     return browser
 
-def get_all_image_element_ids(browser, target):
-    re_expr_clickable = re.compile('<div jsaction=.+?data-id="(.+?)".+?>')
+def get_all_image_element_ids(browser, target, more_images=False):
+    re_expr_clickable = re.compile('<div.*?jsaction="IE7JUb.+?".*?data-id="(.+?)".*?>')
     search_url = 'https://www.google.com/search?'
     img_url =  search_url + 'q={}&tbm=isch'.format(target)
     browser.get(img_url)
     towards_end_of_scroll(browser)
-    # Click more images
-    browser.find_element_by_xpath('//input[@class="mye4qd"]').click()
-    towards_end_of_scroll(browser)
+    if more_images:
+        # Click more images
+        browser.find_element_by_xpath('//input[@class="mye4qd"]').click()
+        towards_end_of_scroll(browser)
     match_results = re_expr_clickable.findall(browser.page_source)
     return match_results
 
 def get_original_image_sources(browser, elem_id):
-    re_expr_img_path = re.compile('<img alt.+?src="([http|https].+?)"')
+    re_expr_img_path = re.compile('<img.*?class="n3VNCb".*?src="([http|https].+?)".*?')
     xpath = '//div[@data-id="{}"]'.format(elem_id)
     browser.find_element_by_xpath(xpath).click()
     time.sleep(1)
@@ -101,7 +104,7 @@ def get_original_image_sources(browser, elem_id):
 def download_url(filename, url, ssl_context):
     headers = {'User-Agent':
                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36'
-               '(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3',
+               '(KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.3',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
                 'Accept-Encoding': 'none',
@@ -124,20 +127,21 @@ def main(args):
     for target_name in targets.split(','):
         target_folder = os.path.join(DOWNLOAD_ROOT, target_name)
         os.makedirs(target_folder, exist_ok=True)
-        element_ids = get_all_image_element_ids(browser, quote(target_name))
+        element_ids = get_all_image_element_ids(browser, quote(target_name), args.more_images)
         for e_idx, elem_id in enumerate(tqdm(element_ids)):
             URLs = get_original_image_sources(browser, elem_id)
-            for u_idx, url in enumerate(URLs):
-                filename = target_folder + '/{}_{}_{}.jpg'
-                filename = filename.format(target_name, e_idx+1, u_idx+1)
-                download_url(filename, url, ssl_context)
+            if len(URLs) > 0:
+                filename = target_folder + '/{}_{}.jpg'
+                filename = filename.format(target_name, e_idx+1)
+                download_url(filename, URLs[0], ssl_context)
 
 if __name__ == '__main__':
     ssl_context = ssl._create_unverified_context()
     parser = argparse.ArgumentParser()
-    # parser.add_argument('-b', '--browser', required=True)
     parser.add_argument('-v', '--version', required=True)
     parser.add_argument('-s', '--search', required=True)
+    parser.add_argument('-m', '--more_images', required=False,
+        type=bool, default=False)
     parser.add_argument('-w', '--window', type=int,
         default=1, help='show browser (default: 1)')
     args = parser.parse_args()
